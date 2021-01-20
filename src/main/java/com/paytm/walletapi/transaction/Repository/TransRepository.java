5fe6a7a1a346de5fe3c67d68e30c377860efd859
Tishya12 <goyaltishya@gmail.com>
2021-01-20 12:43:23 +0530

    Transaction repository updated

--- src/main/java/com/paytm/walletapi/transaction/Repository/TransRepository.java
+++ src/main/java/com/paytm/walletapi/transaction/Repository/TransRepository.java
@@ -1,4 +1,9 @@
 package com.paytm.walletapi.transaction.Repository;
 
-public class TransRepository {
+import com.paytm.walletapi.transaction.Model.TransModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransRepository extends JpaRepository<TransModel,Integer> {
 }

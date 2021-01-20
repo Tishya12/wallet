e6d154f3c4a4722150ffb9c2e2f63de308834576
Tishya12 <goyaltishya@gmail.com>
2021-01-20 12:52:55 +0530

    Transaction controller and service updated

--- src/main/java/com/paytm/walletapi/transaction/Controller/TransController.java
+++ src/main/java/com/paytm/walletapi/transaction/Controller/TransController.java
@@ -1,4 +1,28 @@
 package com.paytm.walletapi.transaction.Controller;
 
+import com.paytm.walletapi.transaction.Model.TransModel;
+import com.paytm.walletapi.transaction.Service.TransService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class TransController {
+    @Autowired
+    TransService transService;
+
+    @GetMapping(value = "/transaction/all")
+    public List<TransModel> displayAll(){
+        return transService.displayall();
+    }
+
+    @PostMapping(value = "/transaction")           // post mapping
+    public String addtransaction(@RequestBody TransModel transModel) {
+        transService.addtransaction(transModel);
+        return "transaction successful";
+    }
 }
--- src/main/java/com/paytm/walletapi/transaction/Service/TransService.java
+++ src/main/java/com/paytm/walletapi/transaction/Service/TransService.java
@@ -1,4 +1,27 @@
 package com.paytm.walletapi.transaction.Service;
 
+import com.paytm.walletapi.transaction.Model.TransModel;
+import com.paytm.walletapi.transaction.Repository.TransRepository;
+import com.paytm.walletapi.wallet.model.WalletModel;
+import com.paytm.walletapi.wallet.repository.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TransService {
+    @Autowired
+    private TransRepository transRepository;
+    public TransModel addtransaction(TransModel transModel) {
+
+        return transRepository.save(transModel);
+    }
+
+    public List<TransModel> displayall() {
+        return transRepository.findAll();
+    }
+
+//    public List<WalletModel> findbyPhone(int phone) {return walletRepository.findByPhone(phone);}
 }
+
